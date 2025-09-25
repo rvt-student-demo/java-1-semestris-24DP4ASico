c4f93c207f2d365dc88ebf51cec8a4b5b9a903e7
24DP4ASico <a240411as@rvt.lv>
2025-09-25 07:50:45 +0000

    Two files

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,16 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli:");
+        double sk = scanner.nextDouble();
+
+        if (sk > 0){
+            System.out.println("Pozitivs");
+        } else {System.out.println("Negativs");}
+
     }
 }
