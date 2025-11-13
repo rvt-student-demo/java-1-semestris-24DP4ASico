1b8c470ed2acead3b8e6b6191e145b9c4cf009a5
24DP4ASico <a240411as@rvt.lv>
2025-11-13 09:00:05 +0000

    Salabots uzdevums chapter60

--- src/main/java/rvt/Chapter60.java
+++ src/main/java/rvt/Chapter60.java
@@ -2,6 +2,11 @@ package rvt;
 
 public class Chapter60 {
     public static void main(String[] args){
+        exs1();
+        exs2();
+        exs3();
+        exs4();
+        exs5();
 
 
     }
@@ -13,7 +18,7 @@ public class Chapter60 {
         }
  
 
-    System.out.println( "Sum of all numbers = " + result );
+        System.out.println( "Sum of all numbers = " + result );
     }
     public static void exs2(){
         int[] val = {13, -4, 82, 17}; 
@@ -24,9 +29,9 @@ public class Chapter60 {
         twice[ 2 ]  = val[ 2 ]*2 ;
         twice[ 3 ]  = val[ 3 ]*2 ;
     
-    System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+        System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
  
-    System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+        System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
     }
     public static void exs3(){
         int[] valA   = { 13, -22,  82,  17}; 
@@ -40,17 +45,17 @@ public class Chapter60 {
 
     
 
-    System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+        System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
     }
     public static void exs4(){
         int[] valA   = { 13, -22,  82,  17}; 
         int[] valB   = {  12, 47,  -57,  8};
     
-    System.out.println( "valA: " + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+        System.out.println( "valA: " + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
  
-    System.out.println( "valB: " + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+        System.out.println( "valB: " + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
 
-    System.out.println( "sum:  "  + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
+        System.out.println( "sum:  "  + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
     }
     public static void exs5(){
         int[] val = {0, 1, 2, 3}; 
