28ec91819e6a7ea291d07af2f225902b9d2953bf
24DP4ASico <a240411as@rvt.lv>
2025-12-05 10:05:07 +0200

    Chapter50(Box) pabeigts

--- src/main/java/rvt/Box.java
+++ src/main/java/rvt/Box.java
@@ -2,70 +2,79 @@ package rvt;
 
 public class Box{
     private double length;
-    private double width;
-    private  double height;
-    private double topArea;
-    private double sideArea;
-    private double faceArea;
+    private double height;
+    private  double width;
 
-
-
-    public Box(double width, double length, double height){
+    public Box(double length, double height, double width){
         this.length = length;
-        this.width = width;
         this.height = height;
+        this.width = width;
     }
+
+    public Box(double side){       
+        this.length = side;
+        this.height = side;
+        this.width = side;
+    } 
 
     public Box(Box oldBox){
-        oldBox.length = this.length;
-        oldBox.width = this.width;
-        oldBox.height = this.height;
-    }
-
-    public Box biggerBox(Box oldBox){
-        return new Box(1.25*oldBox.length,1.25*oldBox.width,
-            1.25*oldBox.height );
-    }
-    public Box smallerBox(Box oldBox){
-        return new Box(0.75*oldBox.length,0.75*oldBox.width,
-         0.75*oldBox.height );
+        this.length = oldBox.length;
+        this.height = oldBox.height;
+        this.width = oldBox.width;
     }
 
 
-    public Box(double side){
-        this(side, side, side);
-    }
-
-    public double volume(){
-        return width*length*height;
-    }
-
-    public double area(){
-        return (width*length) + (width*height) + (length*height);
-    }
-
-    public double getlength(){
+    public double length(){
         return this.length;
     }
 
-    public double getheight(){
+    public double height(){
         return this.height;
     }
 
-    public double getwidth(){
+    public double width(){
         return this.width;
     }
 
-    public double getfaceArea(){
+    public double volume(){
+        return length * height * width;
+    }
+
+    public double area(){
+        return 2 * faceArea() + 2 * sideArea() + 2 * topArea();
+    }
+
+
+    public double faceArea(){
         return length*height;
     }
 
-    public double getsideArea(){
+    public double sideArea(){
         return width*height;
     }
 
-    public double gettopArea(){
+    public double topArea(){
         return width*length;
     }
 
+    public Box biggerBox(Box oldBox){
+        return new Box(
+            1.25 * oldBox.length(),
+            1.25 * oldBox.height(),
+            1.25 * oldBox.width() );
+    }
+    public Box smallerBox(Box oldBox){
+        return new Box(
+            0.75 * oldBox.length(),
+            0.75 * oldBox.height(),
+            0.75 * oldBox.width() );
+    }
+
+    public boolean nests(Box outsideBox)
+{
+        return
+        this.length < outsideBox.length()&&
+        this.height < outsideBox.height() &&
+        this.width < outsideBox.width();
+}
 }
--- src/main/java/rvt/MainBox.java
+++ src/main/java/rvt/MainBox.java
@@ -4,17 +4,23 @@ public class MainBox{
 
   public static void main ( String[] args )
   {
-     Box box = new  Box( 2.5, 5.0, 6.0 ) ;
-     Box box1 = new Box(box);
+    Box box = new Box( 2.5, 5.0, 6.0 ) ;
+    Box big = box.biggerBox(box);
+    Box small = box.smallerBox(box);
      //System.out.println( "Area: "  + box.area() + " volume: " + box.volume() );
 
-      //System.out.println( "length: " + box.getlength() + " height: " + box.getheight() +
-     //                   " width:  " + box.getwidth() )  ;
+      System.out.println( "length: " + box.length() + " height: " + box.height() +
+                        " width:  " + box.width() )  ;
 
-        //System.out.println( "topArea: "  + box.gettopArea() );
-        //System.out.println( "sideArea: "  + box.getsideArea() );
-        //System.out.println( "faceArea: "  + box.getfaceArea() );
-
+        //System.out.println( "topArea: "  + box.topArea() );
+        //System.out.println( "sideArea: "  + box.sideArea() );
+        //System.out.println( "faceArea: "  + box.faceArea() );
+        System.out.println("Bigger length: " + big.length()+ " Bigger height:" + big.height()
+                            + " Bigger width:" + big.width());
+        System.out.println("Smaller length: " + small.length()+ " Smaller height:" + small.height()
+                           + " Smaller width:" + small.width());
+        System.out.println("Small fits in box? " + small.nests(box));
+        System.out.println("Big fits in box? " + big.nests(box));
+        System.out.println("Big fits in small? " + small.nests(big));
   }
 }
-    
