5710ab4743a809981d84a21d67aa336c9dfbe34b
24DP4ASico <a240411as@rvt.lv>
2025-12-08 10:49:32 +0200

    Nav pabeigts Card payments 5-4

--- src/main/java/rvt/MainPaymentCard.java
+++ src/main/java/rvt/MainPaymentCard.java
@@ -6,24 +6,58 @@ public class MainPaymentCard {
     public static void main(String[] args) {
 
         
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
-        
-        paulsCard.eatHeartily();
-        mattsCard.eatAffordably();
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.addMoney(20.00);
-        mattsCard.eatHeartily();
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        mattsCard.addMoney(50);
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-        
-        
+        PaymentCard paulsCard = new PaymentCard(100);
+        PaymentCard mattsCard = new PaymentCard(100);
+
+        //paulsCard.EatHeartily();
+        //mattsCard.EatAffordably();
+        //System.out.println("Paul: " + paulsCard);
+        //System.out.println("Matt: " + mattsCard);
+        //paulsCard.EddMoney(20.00);
+        //mattsCard.EatHeartily();
+        //System.out.println("Paul: " + paulsCard);
+        //System.out.println("Matt: " + mattsCard);
+        //paulsCard.EatAffordably();
+        //paulsCard.EatAffordably();
+        //mattsCard.addMoney(50);
+        //System.out.println("Paul: " + paulsCard);
+        //System.out.println("Matt: " + mattsCard);
+        //System.out.println("money " + mattsCard);
+        //boolean wasSuccessful = mattsCard.takeMoney(75);
+        //System.out.println("successfully withdrew: " + wasSuccessful);
+        //System.out.println("money " + mattsCard);
+
+        //wasSuccessful = paulsCard.takeMoney(125);
+        //System.out.println("successfully withdrew: " + wasSuccessful);
+        //System.out.println("money " + paulsCard);
+        //PaymentTerminal unicafeExactum = new PaymentTerminal();
+
+        //double change = unicafeExactum.eatAffordably(10);
+        //System.out.println("remaining change " + change);
+
+        //change = unicafeExactum.eatAffordably(5);
+        //System.out.println("remaining change " + change);
+
+        //change = unicafeExactum.eatHeartily(4.3);
+        //System.out.println("remaining change " + change);
+
+        //System.out.println(unicafeExactum);
+
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
+
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change: " + change);
+
+        PaymentCard annesCard = new PaymentCard(7);
+
+        boolean wasSuccessful = unicafeExactum.eatHeartily(mattsCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatHeartily(mattsCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatAffordably(mattsCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+
+        System.out.println(unicafeExactum);
 
     }
-          }
+}
--- src/main/java/rvt/PaymentCard.java
+++ src/main/java/rvt/PaymentCard.java
@@ -3,38 +3,52 @@ package rvt;
 public class PaymentCard {
 
     private double balance;
+    private double money;
+    private int affordableMeals;
+    private int heartyMeals;
 
     public PaymentCard(double openingBalance) {
         this.balance = openingBalance;
+        
     }
 
-    public String toString() {
-        return "The card has a balance of " + this.balance + " euros";
+    //public String toString() {
+    //    return "The card has a balance of " + this.balance + " euros";
+    //}
+
+    public boolean EatAffordably() {
+        if (this.balance >= 2.50) {
+            this.balance -= 2.50;
+            return true;
+        }
+        return false;
     }
 
-    public void eatAffordably() {
-        if (this.balance >= 2.60) {
-            this.balance -= 2.60;
+    public boolean EatHeartily() {
+        if (this.balance >= 4.30) {
+            this.balance -= 4.30;
+            return true;
+        }
+        return false;
+    }
+
+    public void addMoney(double increase) {
+        this.balance = this.balance + increase;
+    }
+    public boolean takeMoney(double amount){
+        if(this.balance >= amount){
+            this.balance = this.balance - amount;
+            return true;
+        }
+        else{
+            return false;
         }
     }
-
-    public void eatHeartily() {
-        if (this.balance >= 4.60) {
-            this.balance -= 4.60;
-        }
-    }
-
-    public void addMoney(double amount) {
-        double total;
-        double overage;
-        if (amount > 0) {
-            total = this.balance + amount;
-            overage = total - 150;
-            if (total > 150.00) {
-                total -= overage;
-            }
-            this.balance = total;
-        }
-    }
-
 }
+        
+
+
+
+
+
+
